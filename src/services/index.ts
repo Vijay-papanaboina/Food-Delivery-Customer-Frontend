48a7a71c1167cb86a478cc9260620b19dcf64428
vijay-papanaboina <vijaypapanaboina3@gmail.com>
2025-10-23 14:19:59 +0530

    Refactor delivery-related code by removing unused delivery API and hooks. Updated Layout component to use type imports for better clarity. This cleanup improves code maintainability and reduces unnecessary complexity.

--- src/services/index.ts
+++ src/services/index.ts
@@ -4,7 +4,6 @@ export { UserApi } from "./userApi";
 export { RestaurantApi } from "./restaurantApi";
 export { OrderApi } from "./orderApi";
 export { PaymentApi } from "./paymentApi";
-export { DeliveryApi } from "./deliveryApi";
 
 // Create instances for easy importing
 import { AuthApi } from "./authApi";
@@ -12,11 +11,9 @@ import { UserApi } from "./userApi";
 import { RestaurantApi } from "./restaurantApi";
 import { OrderApi } from "./orderApi";
 import { PaymentApi } from "./paymentApi";
-import { DeliveryApi } from "./deliveryApi";
 
 export const authApi = new AuthApi();
 export const userApi = new UserApi();
 export const restaurantApi = new RestaurantApi();
 export const orderApi = new OrderApi();
 export const paymentApi = new PaymentApi();
-export const deliveryApi = new DeliveryApi();
